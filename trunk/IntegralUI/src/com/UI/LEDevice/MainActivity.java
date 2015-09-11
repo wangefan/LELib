2a1da3c8424f3c97012a23298507f2d035ce3b26
wangefan <wangefan@gmail.com>
2015-09-11 10:46:23 +0000

    [What]Prepare list view

--- trunk/IntegralUI/src/com/UI/LEDevice/MainActivity.java
+++ trunk/IntegralUI/src/com/UI/LEDevice/MainActivity.java
@@ -1,15 +1,29 @@
 package com.UI.LEDevice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.BLE.BLEUtility.BLEUtility;
 import com.BLE.Buttons.BLEButton;
+import com.UI.LEDevice.AnimatedExpandableListView.AnimatedExpandableListAdapter;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends CustomTitleActivity 
@@ -17,7 +31,10 @@ public class MainActivity extends CustomTitleActivity
 	//constant 
 	private final String mTAG = "MainActivity";
 	//data member
+	private AnimatedExpandableListView listView;
+	private ExampleAdapter adapter;
 	
+	//Inner classes
 	BroadcastReceiver mBtnReceiver = new BroadcastReceiver() {
 
 		@Override
@@ -57,14 +74,199 @@ public class MainActivity extends CustomTitleActivity
             }
 		}
 	};
+	
+	private static class GroupItem {
+		String title;
+		List<ChildItem> items = new ArrayList<ChildItem>();
+	}
+
+	private static class ChildItem {
+		String title;
+		//String hint;
+	}
+
+	private static class ChildHolder {
+		TextView title;
+		//TextView hint;
+	}
+
+	private static class GroupHolder {
+		TextView title;
+	}
+
+	/**
+	 * Adapter for our list of {@link GroupItem}s.
+	 */
+	private class ExampleAdapter extends AnimatedExpandableListAdapter {
+		private LayoutInflater inflater;
+
+		private List<GroupItem> items;
+
+		public ExampleAdapter(Context context) {
+			inflater = LayoutInflater.from(context);
+		}
+
+		public void setData(List<GroupItem> items) {
+			this.items = items;
+		}
+
+		@Override
+		public ChildItem getChild(int groupPosition, int childPosition) {
+			return items.get(groupPosition).items.get(childPosition);
+		}
+
+		@Override
+		public long getChildId(int groupPosition, int childPosition) {
+			return childPosition;
+		}
+
+		@Override
+		public View getRealChildView(int groupPosition, int childPosition,
+				boolean isLastChild, View convertView, ViewGroup parent) {
+			ChildHolder holder;
+			ChildItem item = getChild(groupPosition, childPosition);
+			if (convertView == null) {
+				holder = new ChildHolder();
+				convertView = inflater.inflate(R.layout.list_item, parent,
+						false);
+				holder.title = (TextView) convertView
+						.findViewById(R.id.textTitle);
+				/*holder.hint = (TextView) convertView
+						.findViewById(R.id.textHint);*/
+				convertView.setTag(holder);
+			} else {
+				holder = (ChildHolder) convertView.getTag();
+			}
+
+			holder.title.setText(item.title);
+			//holder.hint.setText(item.hint);
+
+			return convertView;
+		}
+
+		@Override
+		public int getRealChildrenCount(int groupPosition) {
+			return items.get(groupPosition).items.size();
+		}
+
+		@Override
+		public GroupItem getGroup(int groupPosition) {
+			return items.get(groupPosition);
+		}
+
+		@Override
+		public int getGroupCount() {
+			return items.size();
+		}
+
+		@Override
+		public long getGroupId(int groupPosition) {
+			return groupPosition;
+		}
+
+		@Override
+		public View getGroupView(int groupPosition, boolean isExpanded,
+				View convertView, ViewGroup parent) {
+			GroupHolder holder;
+			GroupItem item = getGroup(groupPosition);
+			if (convertView == null) {
+				holder = new GroupHolder();
+				convertView = inflater.inflate(R.layout.group_item, parent,
+						false);
+				holder.title = (TextView) convertView
+						.findViewById(R.id.textTitle);
+				convertView.setTag(holder);
+			} else {
+				holder = (GroupHolder) convertView.getTag();
+			}
+
+			holder.title.setText(item.title);
+
+			return convertView;
+		}
+
+		@Override
+		public boolean hasStableIds() {
+			return true;
+		}
+
+		@Override
+		public boolean isChildSelectable(int arg0, int arg1) {
+			return true;
+		}
+
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getActionBar().setTitle("Low Energy Devices");
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		 
 		registerReceiver(mBtnReceiver, makeServiceActionsIntentFilter());	
-		setContentView(R.layout.mainactivity);
+		setContentView(R.layout.activity_expandable_list_view);
+
+		List<GroupItem> items = new ArrayList<GroupItem>();
+
+		// Populate our list with groups and it's children
+		for (int i = 1; i < 100; i++) {
+			GroupItem item = new GroupItem();
+
+			item.title = "Expand this item " + i;
+
+			for (int j = 0; j < i; j++) {
+				ChildItem child = new ChildItem();
+				child.title = "Expanded " + j;
+				//child.hint = "Too awesome";
+
+				item.items.add(child);
+			}
+
+			items.add(item);
+		}
+		
+		
+
+		adapter = new ExampleAdapter(this);
+		adapter.setData(items);
+
+		listView = (AnimatedExpandableListView) findViewById(R.id.list_view);
+		listView.setAdapter(adapter);
+
+		// In order to show animations, we need to use a custom click handler
+		// for our ExpandableListView.
+		listView.setOnGroupClickListener(new OnGroupClickListener() {
+
+			@Override
+			public boolean onGroupClick(ExpandableListView parent, View v,
+					int groupPosition, long id) {
+				// We call collapseGroupWithAnimation(int) and
+				// expandGroupWithAnimation(int) to animate group
+				// expansion/collapse.
+				if (listView.isGroupExpanded(groupPosition)) {
+					listView.collapseGroupWithAnimation(groupPosition);
+				} else {
+					listView.expandGroupWithAnimation(groupPosition);
+				}
+				return true;
+			}
+
+		});
+
+		// Set indicator (arrow) to the right
+		Display display = getWindowManager().getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		int width = size.x;
+		//Log.v("width", width + "");
+		Resources r = getResources();
+		int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+				50, r.getDisplayMetrics());
+		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+			listView.setIndicatorBounds(width - px, width);
+		} else {
+			listView.setIndicatorBoundsRelative(width - px, width);
+		}
 	}
 	
 	private static IntentFilter makeServiceActionsIntentFilter() {

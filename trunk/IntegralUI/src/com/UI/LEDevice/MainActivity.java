7e03d2767aea9000ed46293df9ab308e4ce66f40
wangefan <wangefan@gmail.com>
2015-10-17 14:39:38 +0000

    [What]implement about

--- trunk/IntegralUI/src/com/UI/LEDevice/MainActivity.java
+++ trunk/IntegralUI/src/com/UI/LEDevice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.UI.LEDevice;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.BLE.BLEUtility.BLEDevice;
 import com.BLE.BLEUtility.BLEUtility;
@@ -29,7 +31,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -67,6 +71,12 @@ public class MainActivity extends ActionBarActivity {
 				UIUtility.showProgressDlg(false, R.string.prgsConnted);
 				IntegralSetting.setDeviceName(mPreDevice.getDeviceName());
 				IntegralSetting.setDeviceMACAddr(mPreDevice.getAddress());
+				
+				//Update MAC address
+				String strTleMain = String.format("%s [%s]", getResources().getString(R.string.drawer_title_Main), 
+						IntegralSetting.getDeviceMACAddr());  
+				mDrawerItems.get(0).setTitle(strTleMain);
+				
 				updateUIForConn();
 				Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
         		if(mIntegral != null)
@@ -83,12 +93,55 @@ public class MainActivity extends ActionBarActivity {
 			}
 			else if(BLEUtility.ACTION_UPDATE_ABOUT.equals(action)) {
 				MyLog.d(mTAG, "Update About");
-				String strVer = new String(intent.getStringExtra(BLEUtility.ACTION_UPDATE_ABOUT_VER_KEY));
-				mDrawerItems.get(1).setTitle(strVer);
+				
+				//Update v mode
 				String strVMode = new String(intent.getStringExtra(BLEUtility.ACTION_UPDATE_ABOUT_VMODE_KEY));
-				mDrawerItems.get(2).setTitle(strVMode);
+				String strVmodeFull = getResources().getString(R.string.drawer_title_HDMIVideo_none);
+				if(strVMode.length() > 0) {
+					Character x = strVMode.charAt(7);
+					Character y = strVMode.charAt(9);
+					Character z = strVMode.charAt(11);
+					String vMode1 = (y.compareTo('0') == 0) ? getResources().getString(R.string.aboutVmodeDVI) : getResources().getString(R.string.aboutVmodeHDMI);
+					int nX = Character.getNumericValue(x);
+					String vMode2 = mVmode2Coll.get(nX);
+					String vMode3 = (z.compareTo('0') == 0) ? getResources().getString(R.string.aboutVmode3G) : getResources().getString(R.string.aboutVmode6G);
+					strVmodeFull = String.format("%s %s %s", vMode1, vMode2, vMode3);
+				}
+				mDrawerItems.get(1).setTitle(strVmodeFull);
+				
+				//update link status
 				String strLinkSt = new String(intent.getStringExtra(BLEUtility.ACTION_UPDATE_ABOUT_LINKST_KEY));
-				mDrawerItems.get(3).setTitle(strLinkSt);
+				String strLinkStFull = getResources().getString(R.string.drawer_title_HDMILink);
+				if(strLinkSt.length() > 0) {
+					Character rxTop = strLinkSt.charAt(7);
+					Character rxBot = strLinkSt.charAt(9);
+					Character txTop = strLinkSt.charAt(11);
+					Character txBot = strLinkSt.charAt(13);
+					String strRxTop = (rxTop == '0') ? getResources().getString(R.string.drawer_HDMILink_Nac) :
+						getResources().getString(R.string.drawer_HDMILink_Act);
+					String strRxBot = (rxBot == '0') ? getResources().getString(R.string.drawer_HDMILink_Nac) :
+						getResources().getString(R.string.drawer_HDMILink_Act);
+					String strTxTop = (txTop == '0') ? getResources().getString(R.string.drawer_HDMILink_Nac) :
+						getResources().getString(R.string.drawer_HDMILink_Act);
+					String strTxBot = (txBot == '0') ? getResources().getString(R.string.drawer_HDMILink_Nac) :
+						getResources().getString(R.string.drawer_HDMILink_Act);
+					strLinkStFull = String.format(getResources().getString(R.string.drawer_title_HDMILink), 
+							strRxTop, strRxBot, strTxTop, strTxBot);
+				}
+				mDrawerItems.get(2).setTitle(strLinkStFull);
+				String strVer = new String(intent.getStringExtra(BLEUtility.ACTION_UPDATE_ABOUT_VER_KEY));
+				String strVerFull = getResources().getString(R.string.drawer_title_DEVICEVER_none);
+				if(strVer.length() > 0)
+				{
+					final int nXXStart = 4, nYYStart = 7, nZZStart = 10, nWWStart = 13, nCount = 2;  
+					String xx = strVer.substring(nXXStart, nXXStart + nCount);
+					String yy = strVer.substring(nYYStart, nYYStart + nCount);
+					String zz = strVer.substring(nZZStart, nZZStart + nCount);
+					String ww = strVer.substring(nWWStart, nWWStart + nCount);
+					strVerFull = String.format("FW ver. %s, %s, %s, %s", xx, yy, zz, ww);
+				}
+				
+				mDrawerItems.get(3).setTitle(strVerFull);
 				mAdapter.notifyDataSetChanged();
 			}
 			//Blew are commands relaive 
@@ -189,6 +242,118 @@ public class MainActivity extends ActionBarActivity {
 	private BLEDevice mPreDevice = null;
 	private ArrayList<BLEDevice> mLeDevices = new ArrayList<BLEDevice>();
 	private ExpandaListActivity mIntegral = null;
+    private static final Map<Integer , String> mVmode2Coll;
+    static
+    {
+    	mVmode2Coll = new HashMap<Integer , String>();
+    	mVmode2Coll.put(0, "");
+    	mVmode2Coll.put(1, "640x480p60");
+    	mVmode2Coll.put(2, "720x480p60");
+    	mVmode2Coll.put(3, "720x480p60w");
+    	mVmode2Coll.put(4, "1280x720p60");
+    	mVmode2Coll.put(5, "1920x1080i60");
+    	mVmode2Coll.put(6, "720x480i60");
+    	mVmode2Coll.put(7, "720x480i60w");
+    	mVmode2Coll.put(8, "720x240p60n");
+    	mVmode2Coll.put(9, "720x240p60w");
+    	mVmode2Coll.put(10, "2880x480i60n");
+    	mVmode2Coll.put(11, "2880x480i60w");
+    	mVmode2Coll.put(12, "2880x240p60n");
+    	mVmode2Coll.put(13, "2880x240p60w");
+    	mVmode2Coll.put(14, "1440x480p60n");
+    	mVmode2Coll.put(15, "1440x480p60w");
+    	mVmode2Coll.put(16, "1920x1080p60");
+    	mVmode2Coll.put(17, "720x576p50");
+    	mVmode2Coll.put(18, "720x576p50w");
+    	mVmode2Coll.put(19, "1280x720p50");
+    	mVmode2Coll.put(20, "1920x1080i50");
+    	mVmode2Coll.put(21, "720x576i50");
+    	mVmode2Coll.put(22, "720x576i50w");
+    	mVmode2Coll.put(23, "720x288p50n");
+    	mVmode2Coll.put(24, "720x288p50w");
+    	mVmode2Coll.put(25, "2880x576i50n");
+    	mVmode2Coll.put(26, "2880x576i50w");
+    	mVmode2Coll.put(27, "2880x288p50n");
+    	mVmode2Coll.put(28, "2880x288p50w");
+    	mVmode2Coll.put(29, "1440x576p50n");
+    	mVmode2Coll.put(30, "1440x576p50w");
+    	mVmode2Coll.put(31, "1920x1080p50");
+    	mVmode2Coll.put(32, "1920x1080p24");
+    	mVmode2Coll.put(33, "1920x1080p25");
+    	mVmode2Coll.put(34, "1920x1080p30");
+    	mVmode2Coll.put(35, "2880x480p60n");
+    	mVmode2Coll.put(36, "2880x480p60w");
+    	mVmode2Coll.put(37, "2880x576p50n");
+    	mVmode2Coll.put(38, "2880x576p50w");
+    	mVmode2Coll.put(39, "1920x1080i50");
+    	mVmode2Coll.put(40, "1920x1080i100");
+    	mVmode2Coll.put(41, "1280x720p100");
+    	mVmode2Coll.put(42, "720x576p100n");
+    	mVmode2Coll.put(43, "720x576p100w");
+    	mVmode2Coll.put(44, "720x576i100n");
+    	mVmode2Coll.put(45, "720x576i100w");
+    	mVmode2Coll.put(46, "1920x1080i120");
+    	mVmode2Coll.put(47, "1280x720p120");
+    	mVmode2Coll.put(48, "720x480p120n");
+    	mVmode2Coll.put(49, "720x480p120w");
+    	mVmode2Coll.put(50, "720x480i120n");
+    	mVmode2Coll.put(51, "720x480i120w");
+    	mVmode2Coll.put(52, "720x576p200n");
+    	mVmode2Coll.put(53, "720x576p200w");
+    	mVmode2Coll.put(54, "720x576i200n");
+    	mVmode2Coll.put(55, "720x576i200w");
+    	mVmode2Coll.put(56, "720x480p240n");
+    	mVmode2Coll.put(57, "720x480p240w");
+    	mVmode2Coll.put(58, "720x480i240n");
+    	mVmode2Coll.put(59, "720x480i240w");
+    	mVmode2Coll.put(60, "1280x720p24");
+    	mVmode2Coll.put(61, "1280x720p25");
+    	mVmode2Coll.put(62, "1280x720p30");
+    	mVmode2Coll.put(63, "1920x1080p120");
+    	mVmode2Coll.put(64, "1920x1080p100");
+    	mVmode2Coll.put(65, "1920x1080p100");
+    	mVmode2Coll.put(66, "1920x1080p100");
+    	mVmode2Coll.put(67, "1920x1080p100");
+    	mVmode2Coll.put(68, "1920x1080p100");
+    	mVmode2Coll.put(69, "1920x1080p100");
+    	mVmode2Coll.put(70, "1920x1080p100");
+    	mVmode2Coll.put(71, "1920x1080p100");
+    	mVmode2Coll.put(72, "1920x1080p100");
+    	mVmode2Coll.put(73, "1920x1080p100");
+    	mVmode2Coll.put(74, "1920x1080p100");
+    	mVmode2Coll.put(75, "1920x1080p100");
+    	mVmode2Coll.put(76, "1920x1080p100");
+    	mVmode2Coll.put(77, "1920x1080p100");
+    	mVmode2Coll.put(78, "1920x1080p100");
+    	mVmode2Coll.put(79, "1920x1080p100");
+    	mVmode2Coll.put(80, "1920x1080p100");
+    	mVmode2Coll.put(81, "1680x720p30");
+    	mVmode2Coll.put(82, "1680x720p50");
+    	mVmode2Coll.put(83, "1680x720p60");
+    	mVmode2Coll.put(84, "1680x720p100");
+    	mVmode2Coll.put(85, "1680x720p120");
+    	mVmode2Coll.put(86, "2560x1080p24");
+    	mVmode2Coll.put(87, "2650x1080p25");
+    	mVmode2Coll.put(88, "2650x1080p30");
+    	mVmode2Coll.put(89, "2560x1080p50");
+    	mVmode2Coll.put(90, "2650x1080p60");
+    	mVmode2Coll.put(91, "1080p100");
+    	mVmode2Coll.put(92, "1080p120");
+    	mVmode2Coll.put(93, "3840x2160p24");
+    	mVmode2Coll.put(94, "3840x2160p25");
+    	mVmode2Coll.put(95, "3840x2160p30");
+    	mVmode2Coll.put(96, "3840x2160p50");
+    	mVmode2Coll.put(97, "3840x2160p60");
+    	mVmode2Coll.put(98, "4096x2160p24");
+    	mVmode2Coll.put(99, "4096x2160p25");
+    	mVmode2Coll.put(100, "4096x2160p30");
+    	mVmode2Coll.put(101, "4096x2160p50");
+    	mVmode2Coll.put(102, "4096x2160p60");
+    	mVmode2Coll.put(103, "3840x2160p24");
+    	mVmode2Coll.put(104, "3840x2160p25");
+    	mVmode2Coll.put(105, "3840x2160p30");
+    	mVmode2Coll.put(106, "3840x2160p50");
+    }
 	
 	//Member functions
 	public void updateUIForConn()
@@ -364,7 +529,7 @@ public class MainActivity extends ActionBarActivity {
 		prepareNavigationDrawerItems();
 		mAdapter = new DrawerAdapter(this, mDrawerItems, true);
 		mDrawerList.setAdapter(mAdapter);
-		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+		
 		mDrawerLayout.setDrawerListener(mDrawerToggle);
 
 		mHandler = new Handler();
@@ -411,15 +576,15 @@ public class MainActivity extends ActionBarActivity {
 		mDrawerItems.add(new DrawerItem(R.string.drawer_icon_Main,
 				getResources().getString(R.string.drawer_title_Main),
 				DrawerItem.DRAWER_ITEM_Main));
-		mDrawerItems.add(new DrawerItem(R.string.drawer_icon_DEVICEVER,
-				getResources().getString(R.string.drawer_title_DEVICEVER),
-				DrawerItem.DRAWER_ITEM_DEVICEVER));
 		mDrawerItems.add(new DrawerItem(R.string.drawer_icon_HDMIVideo,
-				getResources().getString(R.string.drawer_title_HDMIVideo),
+				getResources().getString(R.string.drawer_title_HDMIVideo_none),
 				DrawerItem.DRAWER_ITEM_HDMIVideo));
 		mDrawerItems.add(new DrawerItem(R.string.drawer_icon_HDMILink,
 				getResources().getString(R.string.drawer_title_HDMILink),
 				DrawerItem.DRAWER_ITEM_HDMILink));
+		mDrawerItems.add(new DrawerItem(R.string.drawer_icon_DEVICEVER,
+				getResources().getString(R.string.drawer_title_DEVICEVER_none),
+				DrawerItem.DRAWER_ITEM_DEVICEVER));
 		mDrawerItems.add(new DrawerItem(R.string.drawer_icon_LINKHDF,
 				getResources().getString(R.string.drawer_title_LINKHDF),
 				DrawerItem.DRAWER_ITEM_LINKHDF));
@@ -473,15 +638,6 @@ public class MainActivity extends ActionBarActivity {
         default:	
 		}	
 		return super.onOptionsItemSelected(item);
-	}
-
-	private class DrawerItemClickListener implements
-			ListView.OnItemClickListener {
-		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position,
-				long id) {
-			selectItem(position, mDrawerItems.get(position).getTag());
-		}
 	}
 
 	private Fragment selectItem(int position, int drawerTag) {
